390c53a48f0a9fe68138cef0337ce4548f535f15
Usman <usman.zahid@xgrid.co>
2023-06-08 01:30:47 +0500

    Adding 2nd account configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "second_account" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "usmanzahid@copebit.ch"
+    AccountName               = "second-aft"
+    ManagedOrganizationalUnit = "Demo"
+    SSOUserEmail              = "usmanzahid@copebit.ch"
+    SSOUserFirstName          = "Second"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT-2nd"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Usman Zahid"
+    change_reason       = "Provisiong second Account"
+  }
+
+  custom_fields = {
+    group = "prod"
+  }
+
+  account_customizations_name = "sandbox-aft-account"
+}
